ae28e66573e86fa34059059c7ce80eca412e80ae
damar <damar@gitlab.lab>
2022-05-09 13:21:47 +0700

    close server 9001

--- src/api.js
+++ src/api.js
@@ -4,12 +4,12 @@ const http = require('http');
 const serverless = require('serverless-http');
 const { app } = require('./app');
 
-const PORT = process.env.PORT || 9001;
-http.createServer(app)
-.listen(
-    PORT, 
-    () => {
-        console.log(`server listen on port: ${PORT}`);
-    });
+// const PORT = process.env.PORT || 9001;
+// http.createServer(app)
+// .listen(
+//     PORT, 
+//     () => {
+//         console.log(`server listen on port: ${PORT}`);
+//     });
 
 module.exports.handler = serverless(app);
