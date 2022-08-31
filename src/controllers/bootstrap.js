67a88fd9a8c3c46ecbe9cac0939b70e3189989db
damar <damar@gitlab.lab>
2022-08-31 08:27:16 +0700

    fix

--- src/controllers/bootstrap.js
+++ src/controllers/bootstrap.js
@@ -22,7 +22,7 @@ const getRecomendation = async (req, res) => {
             return {
                 id: data.id,
                 name: `${data.first_name} ${data.second_name}`,
-                cost: (data.now_cost / 10),
+                cost: data.now_cost,
                 points: data.total_points,
                 goals: data.goals_scored,
                 assists: data.assists,
