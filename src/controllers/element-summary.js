f1109433025c8ee0a32d460a3f49d1aa1f2ed0ec
damar <damar@gitlab.lab>
2022-10-20 11:19:01 +0700

    add mean total

--- src/controllers/element-summary.js
+++ src/controllers/element-summary.js
@@ -24,15 +24,20 @@ const getPlayerSurplusById = async (req, res) => {
         const id = req.url.split('/')[3];
         const elSummary = await getElementSummary(id);
         let total = 0;
+        let total2 = 2;
+        let mean_total = 0;
         const surplusSummary = elSummary.history.map((data, index) => {
             total += data.total_points;
+            total2 += total;
             mean = parseFloat((total / (index + 1)).toFixed(2))
+            mean_total = parseFloat((total2 / (index + 1)).toFixed(2))
             return {
                 gameweek: data.round,
                 surplus_per_game: parseFloat((data.total_points - (data.value / 10)).toFixed(2)),
                 point_per_value: parseFloat((data.total_points / (data.value / 10)).toFixed(2)),
                 total,
-                mean
+                mean,
+                mean_total
             }
         })
         createResponseBody(res, surplusSummary)
